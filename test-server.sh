8d6a273df6aecc532d0266350897bd2d8d35dea2
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-04-02 12:37:26 +0200

    build(server): extend test script to read from server

--- test-server.sh
+++ test-server.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
+echo "Creating transfer..."
 grpcurl -import-path src/proto -proto transfer.proto -d '{
   "sender":"person-1",
   "receiver":"person-2",
   "amount":10.20,
   "text":"chargeback"
 }' -plaintext localhost:3001 transfer.TransferService.CreateTransfer
+
+echo "Reading transfer..."
+grpcurl -import-path src/proto -proto transfer.proto -d '{
+  "id":"1"
+}' -plaintext localhost:3001 transfer.TransferService.GetTransfer
