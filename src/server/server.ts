1ec1315008c5736d6878a7b6aa37fed3d879114c
Daniel Tieber <daniel.tieber@dynatrace.com>
2024-04-02 10:55:47 +0200

    feat(server): add simple server

---
+++ src/server/server.ts
@@ -0,0 +1,32 @@
+import { Server, ServerCredentials } from '@grpc/grpc-js'
+import { pino } from 'pino'
+
+const server = new Server()
+
+const HOST = process.env.HOST ?? '0.0.0.0'
+const PORT = process.env.PORT ?? '3001'
+const address = `${HOST}:${PORT}`
+
+const logger = pino({
+  level: 'info',
+  messageKey: 'message',
+  transport: {
+    target: 'pino-pretty',
+    options: {
+      messageKey: 'message',
+      colorize: true,
+    },
+  },
+})
+
+server.bindAsync(
+  address,
+  ServerCredentials.createInsecure(),
+  (error, port) => {
+    if (error !== null) {
+      logger.error('Cannot start server')
+      throw error
+    }
+    logger.info(`Server is running on port ${port}`)
+  },
+)
